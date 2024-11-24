2dab428f0fdd010acf6beef8de585008a65f3448
Tommaso-Ferdinandi <tommaso.ferdinandi@yahoo.com>
2024-11-24 15:04:44 +0100

    changed some name to fix email js and worked on the form html and js script

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,20 +1,45 @@
-function sendMail () {
+document.getElementById("contattami").addEventListener("submit", sendMail);
+
+function sendMail(event){
+
+  event.preventDefault();  /*Prevenire crash di default*/
+
+  // ciclo for per selezionare valore radio buttons
+  var modalitaValue = "";
+  var radios = document.getElementsByName("modalita");
+  for (var i = 0; i < radios.length; i++) {
+    if  (radios[i].checked) {
+      modalitaValue = radios[i].value;
+      break
+    }
+  }
+
+  // parametri form
   var params = {
     nome: document.getElementById("nome").value,
     cognome: document.getElementById("cognome").value,
     azienda: document.getElementById("azienda").value,
     email: document.getElementById("email").value,
     motivo: document.getElementById("motivo").value,
-    modalita: document.getElementsByName("modalita").value,
+    modalita: modalitaValue,
     messaggio: document.getElementById("messaggio").value,
   };
+  
+  // parametri emailjs
+  const serviceID = "service_8ie8ywh";
+  const templateID = "template_hsx6o6c";
 
-  emailjs.send("service_8ie8ywh","template_hsx6o6c",params).then(
-    (response) => {
-      console.log('Email inviata!', response.status, response.text);
+  // emailjs send function
+  emailjs.send(serviceID, templateID, params).then(
+    (res) => { 
+      // reset il form dopo invio
+      document.getElementById("contattami").reset();
+      console.log("Email inviata", res);
     },
-    (error) => {
-      console.log('Errore...', error);
-    },
+    (err) => {
+      console.log("Errore invio mail non riuscito", err);
+    }
   );
 }
+
+
